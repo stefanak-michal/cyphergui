468e09bd4f27e50b0d845cf554a89aff7471dc18
Michal Stefanak <stefanak.michal@gmail.com>
2024-04-22 09:06:34 +0200

    migrated result views into own files. graph not yet.

--- src/page/Query.tsx
+++ src/page/Query.tsx
@@ -4,26 +4,19 @@ import { IPageProps } from "../utils/interfaces";
 import db from "../db";
 import { ClipboardContext } from "../utils/contexts";
 import {
-    Date as _Date,
-    DateTime as _DateTime,
-    Duration as _Duration,
-    LocalDateTime as _LocalDateTime,
-    LocalTime as _LocalTime,
     Node as _Node,
     Path as _Path,
     Record,
     Relationship as _Relationship,
     ResultSummary,
-    Time as _Time,
 } from "neo4j-driver";
 import { Ecosystem, EPage, EQueryView } from "../utils/enums";
-import { settings } from "../layout/Settings";
 import { Orb, OrbEventType } from "@memgraph/orb";
-import InlineNode from "../components/InlineNode";
-import InlineRelationship from "../components/InlineRelationship";
-import { durationToString, toJSON } from "../utils/fn";
 import orb_logo from "../assets/orb_logo.png";
 import { t_StashQuery } from "../utils/types";
+import Table from "./query/Table";
+import Summary from "./query/Summary";
+import Json from "./query/Json";
 
 interface IQueryProps extends IPageProps {
     query?: string;
@@ -318,28 +311,11 @@ class Query extends React.Component<IQueryProps, IQueryState> {
                     </div>
                 </div>
 
-                <div className={"block " + (this.state.view === EQueryView.Table && this.state.rows.length ? "" : "is-hidden")}>
-                    <div className="table-container">
-                        <table className="table is-bordered is-striped is-narrow is-hoverable">
-                            <thead>
-                                <tr>
-                                    {this.state.keys.map(key => (
-                                        <th key={key}>{key}</th>
-                                    ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.state.rows.map((row, i) => (
-                                    <tr key={i}>
-                                        {this.state.keys.map(key => (
-                                            <td key={key}>{row.has(key) ? this.printValue(row.get(key)) : ""}</td>
-                                        ))}
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                {this.state.view === EQueryView.Table && this.state.rows.length && <Table
+                    keys={this.state.keys}
+                    rows={this.state.rows}
+                    tableSize={this.state.tableSize}
+                    tabManager={this.props.tabManager} />}
 
                 <div className={"block " + (this.state.view === EQueryView.Graph && this.state.rows.length ? "" : "is-hidden")}>
                     <div className="graph" ref={this.graphElement}>
@@ -375,115 +351,14 @@ class Query extends React.Component<IQueryProps, IQueryState> {
                     </div>
                 </div>
 
-                <div className={"block " + (this.state.view === EQueryView.JSON && this.state.rows.length ? "" : "is-hidden")}>
-                    <div className="control has-icons-right">
-                        <pre>{toJSON(this.state.rows)}</pre>
-                        <ClipboardContext.Consumer>
-                            {copy => (
-                                <span className="icon is-right is-clickable" onClick={copy}>
-                                    <i className="fa-regular fa-copy" />
-                                </span>
-                            )}
-                        </ClipboardContext.Consumer>
-                    </div>
-                </div>
+                {this.state.view === EQueryView.JSON && this.state.rows.length && <Json rows={this.state.rows} />}
 
-                <div className={"block " + (this.state.view === EQueryView.Summary && this.state.summary ? "" : "is-hidden")}>
-                    <div className="control has-icons-right">
-                        <pre>{toJSON(this.state.summary)}</pre>
-                        <ClipboardContext.Consumer>
-                            {copy => (
-                                <span className="icon is-right is-clickable" onClick={copy}>
-                                    <i className="fa-regular fa-copy" />
-                                </span>
-                            )}
-                        </ClipboardContext.Consumer>
-                    </div>
-                </div>
+                {this.state.view === EQueryView.Summary && this.state.summary && <Summary summary={this.state.summary} />}
 
                 {this.state.rows.length === 0 && !this.state.summary && <div className="block">No result</div>}
             </>
         );
     }
-
-    printValue = (value: any): React.ReactElement => {
-        if (db.isInt(value)) return <>{db.strInt(value)}</>;
-        if (Array.isArray(value)) return <>[
-                {value.length ? value
-                .map<React.ReactNode>(entry => this.printValue(entry))
-                .reduce((prev, curr) => [prev, ", ", curr]) : ""}
-            ]</>;
-        if (value === null) return <p className="has-text-grey">null</p>;
-        if (typeof value === "boolean") return <>{value ? "true" : "false"}</>;
-        if (typeof value === "string") return <p className="wspace-pre is-inline-block">{value}</p>;
-
-        if (value instanceof _Node) {
-            return <InlineNode node={value} tabManager={this.props.tabManager} small={this.state.tableSize === 1} />;
-        }
-        if (value instanceof _Relationship) {
-            return <InlineRelationship rel={value} tabManager={this.props.tabManager} small={this.state.tableSize === 1} />;
-        }
-        if (value instanceof _Path) {
-            let start = value.start;
-            let first = true;
-            return (
-                <div className="is-flex is-align-items-center">
-                    {value.segments.map(segment => {
-                        const r = (
-                            <>
-                                {first && (
-                                    <>
-                                        <span className="is-size-4">(</span>
-                                        {this.printValue(db.strInt(segment.start.identity) === db.strInt(start.identity) ? segment.start : segment.end)}
-                                        <span className="is-size-4">)</span>
-                                    </>
-                                )}
-                                <span className="is-size-4 wspace-nowrap">{db.strInt(segment.start.identity) === db.strInt(start.identity) ? "-" : "<-"}[</span>
-                                {this.printValue(segment.relationship)}
-                                <span className="is-size-4 wspace-nowrap">]{db.strInt(segment.start.identity) === db.strInt(start.identity) ? "->" : "-"}(</span>
-                                {this.printValue(db.strInt(segment.start.identity) === db.strInt(start.identity) ? segment.end : segment.start)}
-                                <span className="is-size-4">)</span>
-                            </>
-                        );
-                        start = segment.end;
-                        first = false;
-                        return r;
-                    })}
-                </div>
-            );
-        }
-
-        // Temporal values
-        if (value instanceof _Date) {
-            const fn = settings().temporalValueToStringFunction === "toString" ? "toDateString" : settings().temporalValueToStringFunction;
-            return <p className="wspace-nowrap">{value.toStandardDate()[fn]()}</p>;
-        }
-        if (value instanceof _DateTime) return <p className="wspace-nowrap">{value.toStandardDate()[settings().temporalValueToStringFunction]()}</p>;
-        if (value instanceof _Time) return <p className="wspace-nowrap">{value.toString()}</p>;
-        if (value instanceof _LocalDateTime) return <p className="wspace-nowrap">{value.toStandardDate().toLocaleString()}</p>;
-        if (value instanceof _LocalTime) return <p className="wspace-nowrap">{value.toString()}</p>;
-        if (value instanceof _Duration) return <p className="wspace-nowrap">{durationToString(value)}</p>;
-
-        if (typeof value === "object") {
-            return (
-                <>
-                    {value.constructor.name || ""}
-                    <div className="control has-icons-right">
-                        <pre>{toJSON(value)}</pre>
-                        <ClipboardContext.Consumer>
-                            {copy => (
-                                <span className="icon is-right is-clickable" onClick={copy}>
-                                    <i className="fa-regular fa-copy" />
-                                </span>
-                            )}
-                        </ClipboardContext.Consumer>
-                    </div>
-                </>
-            );
-        }
-
-        return value.toString();
-    };
 }
 
 export default Query;

a534b60a466ac579f722c77ec131698382dfe032
Michal Stefanak <stefanak.michal@gmail.com>
2024-04-30 08:16:01 +0200

    modified threshold for dark color

--- src/page/query/graph/SidebarContent.tsx
+++ src/page/query/graph/SidebarContent.tsx
@@ -19,7 +19,7 @@ class SidebarContent extends React.Component<ISidebarProps, {}> {
         const r = (rgb >> 16) & 0xff;  // extract red
         const g = (rgb >>  8) & 0xff;  // extract green
         const b = (rgb >>  0) & 0xff;  // extract blue
-        return (0.2126 * r + 0.7152 * g + 0.0722 * b) < 128; // per ITU-R BT.709
+        return (0.2126 * r + 0.7152 * g + 0.0722 * b) < 175; // per ITU-R BT.709
     }
 
     render() {

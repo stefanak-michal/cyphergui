dcbdeb34541efcbe00c72d62827374c3ada315e3
Michal Stefanak <stefanak.michal@gmail.com>
2024-05-16 11:10:39 +0200

    temporary disabled some workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,9 +13,9 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     timeout: 120000,

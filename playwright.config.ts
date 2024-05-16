d9028d30e23cd7ccf701a35351663391dcd11108
Michal Stefanak <stefanak.michal@gmail.com>
2024-05-16 13:11:03 +0200

    enable trace

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: 0,
+    retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
     workers: undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -24,7 +24,7 @@ export default defineConfig({
         /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: 'http://127.0.0.1:3000',
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry',
+        trace: 'retain-on-failure',
     },
     /* Configure projects for major browsers */
     projects: [

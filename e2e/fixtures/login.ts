c7e18e10c556df0f813b141cb22209e6f6aebb1f
Michal Stefanak <stefanak.michal@gmail.com>
2024-05-16 13:59:32 +0200

    fixed login with empty env vars

--- e2e/fixtures/login.ts
+++ e2e/fixtures/login.ts
@@ -4,8 +4,8 @@ export const test = base.extend({
     page: async ({ page }, use) => {
         await page.goto(process.env.URL || '/');
         await page.getByLabel('URL').fill(process.env.DB_HOSTNAME || 'bolt://localhost:7687');
-        await page.getByLabel('Username').fill(process.env.DB_USERNAME);
-        await page.getByLabel('Password').fill(process.env.DB_PASSWORD);
+        await page.getByLabel('Username').fill(process.env.DB_USERNAME || '');
+        await page.getByLabel('Password').fill(process.env.DB_PASSWORD || '');
         await page.getByRole('button', { name: 'Login' }).click();
         await expect(page.locator('#basicNavbar')).toBeVisible();
 

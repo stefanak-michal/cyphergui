9eab8cef48d98f2f3ed1f4c9207a836ad44470f6
Michal Stefanak <stefanak.michal@gmail.com>
2025-02-19 16:09:46 +0100

    removed redundancy

--- e2e/neo4j-read/history.spec.ts
+++ e2e/neo4j-read/history.spec.ts
@@ -19,12 +19,6 @@ test.describe('History tab', { tag: '@neo4j-read' }, () => {
         await expect(containerLocator(page, 'table tbody')).toContainText(
             'MATCH ()-[n]-() RETURN collect(DISTINCT type(n)) AS c'
         );
-        await expect(containerLocator(page, 'table tbody')).toContainText(
-            'MATCH (n) WITH DISTINCT labels(n) AS ll UNWIND ll AS l RETURN collect(DISTINCT l) AS c'
-        );
-        await expect(containerLocator(page, 'table tbody')).toContainText(
-            'MATCH ()-[n]-() RETURN collect(DISTINCT type(n)) AS c'
-        );
     });
 
     test('Open query', async ({ page }) => {

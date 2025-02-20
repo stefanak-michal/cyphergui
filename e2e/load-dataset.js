54132604c6a3d02e575f72f0a15e7eaca7a8c1ff
Michal Stefanak <stefanak.michal@gmail.com>
2025-02-20 15:10:47 +0100

    tired of messages when I'm trying to make something work in CI

--- e2e/load-dataset.js
+++ e2e/load-dataset.js
@@ -4,29 +4,38 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const uri = process.env.DB_HOSTNAME || 'bolt://localhost:7687';
-const user = process.env.DB_USERNAME || 'neo4j';
+const username = process.env.DB_USERNAME || 'neo4j';
 const password = process.env.DB_PASSWORD;
-
-const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
-const session = driver.session();
-
 const cypherUrl = 'https://raw.githubusercontent.com/neo4j-graph-examples/movies/main/scripts/movies.cypher';
 
 async function loadAndRunCypherQueries() {
+    let session, driver;
     try {
+        driver = neo4j.driver(uri, username.length > 0 && password.length > 0 ? neo4j.auth.basic(username, password) : undefined, {
+            userAgent: 'stefanak-michal/cypherGUI',
+            telemetryDisabled: true
+        });
+        const serverInfo = await driver.getServerInfo()
+        console.log('Connection established')
+        console.log(serverInfo)
+
         const response = await fetch(cypherUrl);
         const cypherQueries = await response.text();
         const queries = cypherQueries.split(';').filter(query => query.trim() !== '');
 
+        session = driver.session({
+            defaultAccessMode: neo4j.session.WRITE
+        });
         for (const query of queries) {
             await session.run(query);
             console.log(`Executed query: ${query}`);
         }
     } catch (error) {
         console.error('Error loading or executing cypher queries:', error);
+        process.exit(1);
     } finally {
-        await session.close();
-        await driver.close();
+        await session?.close();
+        await driver?.close();
     }
 }
 
